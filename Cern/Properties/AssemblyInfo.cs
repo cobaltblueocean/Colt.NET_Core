4858fd1d213c3582dea5aa232c45e5edee60116c
Kei <cobaltblue.ocean@gmail.com>
2024-05-16 11:17:25 -0400

    Updated reference

--- Cern/Properties/AssemblyInfo.cs
+++ Cern/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.9.3.29")]
-[assembly: AssemblyFileVersion("0.9.3.29")]
+[assembly: AssemblyVersion("0.9.3.30")]
+[assembly: AssemblyFileVersion("0.9.3.30")]
